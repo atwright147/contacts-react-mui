cc7ac3bbb5144b9ba86708047e7b6ccc1a7348e7
Andy Wright <atwright147@gmail.com>
2023-11-12 11:56:44 +0000

    customise theme

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from './theme.ts';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -34,7 +36,9 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <CssBaseline />
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/theme.ts
@@ -0,0 +1,30 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  typography: {
+    h1: {
+      fontSize: '3rem',
+      fontWeight: 'normal',
+    },
+    h2: {
+      fontSize: '2.5rem',
+      fontWeight: 'normal',
+    },
+    h3: {
+      fontSize: '2rem',
+      fontWeight: 'normal',
+    },
+    h4: {
+      fontSize: '1.75rem',
+      fontWeight: 'normal',
+    },
+    h5: {
+      fontSize: '1.5rem',
+      fontWeight: 'normal',
+    },
+    h6: {
+      fontSize: '1.25rem',
+      fontWeight: 'normal',
+    },
+  }
+});
