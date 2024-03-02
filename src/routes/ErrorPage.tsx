7df055bce12e30ba98088046adfee01927b45048
Andy Wright <atwright147@gmail.com>
2024-03-02 23:32:36 +0000

    remove eslint config file

--- src/routes/ErrorPage.tsx
+++ src/routes/ErrorPage.tsx
@@ -9,7 +9,6 @@ export function ErrorPage() {
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        {/* eslint-disable-next-line */}
         {/* @ts-ignore */}
         <em>{error.statusText || error.message}</em>
       </p>
