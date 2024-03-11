94ef6726689dedcdb58576e3a12ffd758a4fb71d
Andy Wright <atwright147@gmail.com>
2024-03-11 21:08:44 +0000

    remove useless styles from list

--- src/components/ContactsList.tsx
+++ src/components/ContactsList.tsx
@@ -15,38 +15,38 @@ export const ContactsList = () => {
   };
 
   return (
-      <Box>
-        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.Box' }} dense>
-          {contacts?.map((contact) => (
-            <ListItem key={contact.id} id={`contact-${contact.id}`}>
-              <ListItemButton
-                alignItems="flex-start"
-                selected={Number(id) === contact.id}
-                component="button"
-                onClick={() => handleContactClick(contact.id)}
-              >
-                <ListItemAvatar>
-                  <Avatar
-                    alt={getInitials(`${contact.firstName} ${contact.lastName}`)}
-                    src={`http://localhost:3001/api/v1/avatar/${contact.id}`}
-                    variant="rounded"
-                  />
-                </ListItemAvatar>
-                <ListItemText
-                  primary={
-                    <Stack direction="row" spacing={1} justifyContent="space-between">
-                      <Typography>
-                        {`${contact.firstName} ${contact.lastName}`} {contact.id}
-                      </Typography>
-                      <Favourite isFavourite={!!contact.isFavourite} />
-                    </Stack>
-                  }
-                  secondary={<Typography variant="body2">{contact.jobTitle}</Typography>}
+    <Box>
+      <List dense>
+        {contacts?.map((contact) => (
+          <ListItem key={contact.id} id={`contact-${contact.id}`}>
+            <ListItemButton
+              alignItems="flex-start"
+              selected={Number(id) === contact.id}
+              component="button"
+              onClick={() => handleContactClick(contact.id)}
+            >
+              <ListItemAvatar>
+                <Avatar
+                  alt={getInitials(`${contact.firstName} ${contact.lastName}`)}
+                  src={`http://localhost:3001/api/v1/avatar/${contact.id}`}
+                  variant="rounded"
                 />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Box>
+              </ListItemAvatar>
+              <ListItemText
+                primary={
+                  <Stack direction="row" spacing={1} justifyContent="space-between">
+                    <Typography>
+                      {`${contact.firstName} ${contact.lastName}`} {contact.id}
+                    </Typography>
+                    <Favourite isFavourite={!!contact.isFavourite} />
+                  </Stack>
+                }
+                secondary={<Typography variant="body2">{contact.jobTitle}</Typography>}
+              />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
   );
 };
